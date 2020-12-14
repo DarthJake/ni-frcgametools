88203b7147707bff9fd6c950671bb563b7790bcd
DarthJake <metheamazingwebprogrammer@gmail.com>
2020-12-14 16:21:01 -0700

    Added the nessesary installer file name to the chocolateyinstall.ps1 packageArgs.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,13 +2,14 @@
 # Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/19.0/offline/ni-frc-2020-game-tools_19.0.0_offline.iso'
+$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/19.0/offline/ni-frc-2020-game-tools_19.0.0_offline.iso"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'NISoftware*'
   fileType       = 'EXE' 
   Url            = $url
+  file           = 'Install.exe'
   checksum       = '63da2e87bd66c6003daa2e31283207eecaf2a91dbdae2b060848ad95ab63c094'
   checksumType   = 'sha256'
   silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
