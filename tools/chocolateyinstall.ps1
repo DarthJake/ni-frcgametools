af6cf2fe778e82155591861fa85cc40b14f3a5f9
DarthJake <metheamazingwebprogrammer@gmail.com>
2020-12-14 15:11:24 -0700

    Initial commit.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+# Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/19.0/offline/ni-frc-2020-game-tools_19.0.0_offline.iso'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'NISoftware*'
+  fileType       = 'EXE' 
+  Url            = $url
+  checksum       = '63da2e87bd66c6003daa2e31283207eecaf2a91dbdae2b060848ad95ab63c094'
+  checksumType   = 'sha256'
+  silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
+  validExitCodes = @(0, -125071)
+}
+
+Install-ChocolateyIsoPackage @packageArgs #https://docs.chocolatey.org/en-us/guides/create/mount-an-iso-in-chocolatey-package
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+# The directory for the ni unistaller exe is stored in a reg key
+$uninstallerLocation = Join-Path (Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\National Instruments\Common\Installer\MIF\MetaUninstaller' -Name 'Path') "uninst.exe"
+$systemDriveLetter = (Get-WmiObject Win32_OperatingSystem).getPropertyValue("SystemDrive")
+$niProgramDataLocation = Join-Path $systemDriveLetter "\ProgramData\National Instruments"
+
+$packageArgs = @{
+  PackageName    = $env:ChocolateyPackageName
+  FileType       = "exe"
+  SilentArgs     = "/qb /x all"
+  File           = $uninstallerLocation
+  validExitCodes = @(0, 3010)
+}
+
+Write-Host "`nUninstalling NI FRC Game Tools"
+Write-Host "`n`tRunning the NI uninstaller..."
+Uninstall-ChocolateyPackage  @packageArgs
+
+Write-Host "`n`tRemoving the NI ProgramData folder..." #The uninstaller does not remove this
+if (Test-Path -Path $niProgramDataLocation) {
+  Remove-Item $niProgramDataLocation -ErrorAction SilentlyContinue -Force -Recurse
+  
+  if (!(Test-Path -Path $niProgramDataLocation)) {
+    Write-Host "`n`t`tRemoved the NI ProgramData folder at `"$niProgramDataLocation`"" -ForegroundColor Green
+  } else {
+    Write-Host "`n`t`tAn error occured while trying to delete the NI ProgramData folder at `"$niProgramDataLocation`"" -ForegroundColor Red
+  }
+} else {
+  Write-Host "It appears that the NI ProgramData folder expected at `"$niProgramDataLocation`" does not exists. This could either mean NI was never installed or the uninstaller removed it like it should, but doesn't." -ForegroundColor Yellow
+}
