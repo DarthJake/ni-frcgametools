6f00d58df31a3ec27b7c2e05869e7803bcf4300c
DarthJake <metheamazingwebprogrammer@gmail.com>
2021-10-12 12:21:18 -0700

    Commiting V20 because I forgot...

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 # Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/19.0/offline/ni-frc-2020-game-tools_19.0.0_offline.iso"
+$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/20.0/offline/ni-frc-2020-game-tools_20.0.0_offline.iso"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType       = 'EXE' 
   Url            = $url
   file           = 'Install.exe'
-  checksum       = '63da2e87bd66c6003daa2e31283207eecaf2a91dbdae2b060848ad95ab63c094'
+  checksum       = 'a75e1fb30c441790a5f63cef0df5de16a07d45291313b011371bec7e95f777ab'
   checksumType   = 'sha256'
   silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
   validExitCodes = @(0, -125071, -126300)
