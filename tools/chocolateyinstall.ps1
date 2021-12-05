8be15238defa5b380c734b2573198afd5f41d8aa
Fritz Geib <fritz@standland.org>
2021-12-04 19:56:52 -0500

    update to version 20.0.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 # Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/20.0/offline/ni-frc-2020-game-tools_20.0.0_offline.iso"
+$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/20.0/offline/ni-frc-2020-game-tools_20.0.1_offline.iso"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType       = 'EXE' 
   Url            = $url
   file           = 'Install.exe'
-  checksum       = 'a75e1fb30c441790a5f63cef0df5de16a07d45291313b011371bec7e95f777ab'
+  checksum       = '6bb49ad274d75a3eed4a89586569d69cb68ee2a7a21ed4e1258bc948490b265b'
   checksumType   = 'sha256'
   silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
   validExitCodes = @(0, -125071, -126300)
