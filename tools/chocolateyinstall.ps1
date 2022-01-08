dfd05c9cf99ab6dac2cea33a0a9305068c8cc2a2
Fritz Geib <fritz@standland.org>
2022-01-08 11:56:12 -0500

    PROG-103 update to version 22.0.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 # Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/20.0/offline/ni-frc-2020-game-tools_20.0.1_offline.iso"
+$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2022-game-tools/22.0/offline/ni-frc-2022-game-tools_22.0.0_offline.iso"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType       = 'EXE' 
   Url            = $url
   file           = 'Install.exe'
-  checksum       = '6bb49ad274d75a3eed4a89586569d69cb68ee2a7a21ed4e1258bc948490b265b'
+  checksum       = 'e26caf9f9eb79999a865bd7a695ef50758a58581e74216502f1c55730b20c11e'
   checksumType   = 'sha256'
   silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
   validExitCodes = @(0, -125071, -126300)
