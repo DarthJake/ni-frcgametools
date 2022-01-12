cd4ac5048f8bd86b1023dd6cf83860ea2890313c
DarthJake <metheamazingwebprogrammer@gmail.com>
2022-01-11 23:29:14 -0700

    Update to 2022 Game Tools.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,15 +2,15 @@
 # Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2020-game-tools/20.0/offline/ni-frc-2020-game-tools_20.0.0_offline.iso"
+$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2022-game-tools/22.0/offline/ni-frc-2022-game-tools_22.0.0_offline.iso"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   softwareName   = 'NISoftware*'
-  fileType       = 'EXE' 
+  fileType       = 'EXE'
   Url            = $url
   file           = 'Install.exe'
-  checksum       = 'a75e1fb30c441790a5f63cef0df5de16a07d45291313b011371bec7e95f777ab'
+  checksum       = 'f261a2d051d54f5f272764461d44f05f5033bde734f90ca0fb41c28c94723982'
   checksumType   = 'sha256'
   silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
   validExitCodes = @(0, -125071, -126300)
