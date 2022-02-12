11548a3ca379b1205c27e7542112485be2556c8c
DarthJake <metheamazingwebprogrammer@gmail.com>
2022-02-11 17:33:06 -0700

    Updated to NI FRC Game Tools 22.0.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 # Note: Install programming environments such as NI LabVIEW or Microsoft Visual Studio before installing this product.
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2022-game-tools/22.0/offline/ni-frc-2022-game-tools_22.0.0_offline.iso"
+$url = "https://download.ni.com/support/nipkg/products/ni-f/ni-frc-2022-game-tools/22.0/offline/ni-frc-2022-game-tools_22.0.1_offline.iso"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -10,14 +10,14 @@ $packageArgs = @{
   fileType       = 'EXE'
   Url            = $url
   file           = 'Install.exe'
-  checksum       = 'f261a2d051d54f5f272764461d44f05f5033bde734f90ca0fb41c28c94723982'
+  checksum       = '62401e5ff6ec71c4c3e70f2a239ae312b1b471ee6da6c1a715218b4522161812'
   checksumType   = 'sha256'
   silentArgs     = '--passive --accept-eulas --prevent-reboot --prevent-activation'
   validExitCodes = @(0, -125071, -126300)
 }
 
 if (!(Get-OSArchitectureWidth -Compare 64) -or !($env:OS_NAME -eq "Windows 10" -or $env:OS_NAME -eq "Windows 11")) {
-  throw "NI FRC Game Tools requires Windows 10 64-bit version or newer. Aborting installation. "
+  throw "NI FRC Game Tools requires Windows 10 64-bit version or newer. Aborting installation."
 }
 
 Install-ChocolateyIsoPackage @packageArgs #https://docs.chocolatey.org/en-us/guides/create/mount-an-iso-in-chocolatey-package
